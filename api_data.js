029c04e887d7675a152169180dce33784208589b
Konstantin Kuznetcov <K.Kuznetcov@gmail.com>
2014-11-14 01:58:40 +0000

    First commit

---
+++ api_data.js
@@ -0,0 +1,11 @@
+define({ api: [
+  {
+    "type": "get",
+    "url": "/init",
+    "title": "Вызывается из предзагрузчика игры. Возвращает набор данных для начала игры.",
+    "name": "Init",
+    "group": "Init",
+    "version": "0.0.0",
+    "filename": "controllers/InitController.php"
+  }
+] });
